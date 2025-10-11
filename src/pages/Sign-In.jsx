65dd215780d0e0b408d5ea6f91800805a0fbfa3e
Datodia <dato.diasamidze.02@gmail.com>
2025-10-11 16:09:16 +0400

    add google auth

--- src/pages/Sign-In.jsx
+++ src/pages/Sign-In.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, useParams, useSearchParams } from 'react-router-dom'
 import Cookies from 'js-cookie'
 import { toast } from 'react-toastify'
+import { useEffect } from 'react'
 
 export default function SignIn() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
+    const [searchParams, setSearchParams] = useSearchParams();
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        try{
+        try {
             setLoading(true)
             const resp = await fetch(`${import.meta.env.VITE_SERVER_URL}/auth/sign-in`, {
                 method: 'POST',
@@ -28,46 +30,56 @@ export default function SignIn() {
             console.log(resp)
             console.log(data)
 
-            if(resp.status === 200){
-                Cookies.set('token', data, {expires: 1 / 24})
+            if (resp.status === 200) {
+                Cookies.set('token', data, { expires: 1 / 24 })
                 toast.success('Logged in scuccessly')
                 navigate('/')
-            }else{
+            } else {
                 toast.error(data.message)
                 console.log(data)
             }
-        }catch(e){
+        } catch (e) {
             toast.error(e.message)
-        }finally{
+        } finally {
             setLoading(false)
         }
     }
+
+    useEffect(() => {
+        if (searchParams.get('token')) {
+            Cookies.set('token', searchParams.get('token'), { expires: 60 * 60 })
+            toast.success('Logged in scuccessly')
+            navigate('/')
+        }
+    }, [])
 
     return (
         <div className='flex flex-col justify-center items-center h-screen'>
             <h1>Sign-in</h1>
 
             <form onSubmit={handleSubmit} className='flex flex-col w-[400px] gap-2'>
-                 <input 
-                    type="email" 
-                    placeholder='email' 
-                    value={email} 
-                    onChange={(e) => setEmail(e.target.value)} 
+                <input
+                    type="email"
+                    placeholder='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                     className='border-2 border-black'
                 />
-                 <input 
-                    type="password" 
-                    placeholder='********' 
-                    value={password} 
-                    onChange={(e) => setPassword(e.target.value)} 
+                <input
+                    type="password"
+                    placeholder='********'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     required
                     className='border-2 border-black'
                 />
 
-                <button className='p-2 bg-blue-500'>{loading ? 'loading..': 'Sign-in'}</button>
-                <h2>dont have an account? <Link to={'/sign-up'}>Sign-up</Link></h2>
+                <button className='p-2 bg-blue-500'>{loading ? 'loading..' : 'Sign-in'}</button>
             </form>
+            <Link to={'http://localhost:3000/auth/google'}>Continue With google</Link>
+
+            <h2>dont have an account? <Link to={'/sign-up'}>Sign-up</Link></h2>
         </div>
     )
 }
